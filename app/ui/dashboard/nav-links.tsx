c77bdca62e524052c7fe77fa259baaea3926a70d
Joe <joe@Joes-MacBook-Pro.local>
2023-10-30 13:01:14 -0400

    updates

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -19,6 +19,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
+  { name: "Foobar", href: "/dashboard/foobar", icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
